715f7e09d91f96541de65ac513a3f4a2a16f51c5
Hussien12Mostafa <hussienmostafa8888@gmail.com>
2021-10-05 13:51:53 +0200

    separateq class calc

---
+++ calc.dart
@@ -0,0 +1,6 @@
+class Calc {
+  int add(int a, int b) => a + b;
+  int sub(int a, int b) => a - b;
+  int mult(int a, int b) => a * b;
+  int div(int a, int b) => a ~/ b;
+}
--- task1.dart
+++ task1.dart
@@ -1,11 +1,5 @@
 import 'dart:io';
-
-class Calc {
-  int add(int a, int b) => a + b;
-  int sub(int a, int b) => a - b;
-  int mult(int a, int b) => a * b;
-  int div(int a, int b) => a ~/ b;
-}
+import 'calc.dart';
 
 void main() {
   while (true) {
